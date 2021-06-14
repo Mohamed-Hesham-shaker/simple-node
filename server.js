e60def6ccaf40009a14166380420a3870afa4e26
Mohamed Hesham <mohamed.hesham@thirdwayv.com>
2021-06-14 21:14:30 +0200

    Change server message.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
-    await sleep(5000);
-  }
+    while (true) {
+        console.log('Deployed');
+        await sleep(5000);
+    }
 }
 
 main();
