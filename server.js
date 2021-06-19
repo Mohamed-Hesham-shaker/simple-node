06f8f40ba0180135cd0dc04ed5973db6955c4c99
Mohamed Hesham <mohamed.hesham@thirdwayv.com>
2021-06-19 15:19:52 +0200

    update Server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
-});
+async function main() {
+  while(true) {
+    console.log('Containers rule!');
+    await sleep(5000);
+  }
+}
 
-app.get("/health", (req, res, next) => {
-  res.status(200).send("Hello!");
-});
-
+main();
